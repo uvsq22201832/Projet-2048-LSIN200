741a6210dd58470d333b020c9a44b02513e6c015
Tyson R <tyson.kk@ens.uvsq.fr>
2023-03-24 10:28:37 +0100

    maj

--- 2048  fonctions logique à relier entre elles.py
+++ 2048  fonctions logique à relier entre elles.py
@@ -105,41 +105,7 @@ def état_de_la_partie(mat):
     # Sinon si toute aucunes cases n'est vide et qu'aucun déplacement n'est possible alors
     return 'Tu as perdu !'
 
- 
-# fonction de compression de la grille après chaque étapes avant et après la fusion des cellules.
-# (on utilisera le mot"cellule" je trouve que c'est plus approprié)
-def compress(mat):
- 
-    # variable bool pour déterminer si un changement s'est produit ou non
-    change = False
- 
-    # grille vide avec toute les cellules vides
-    nouvelle_mat =[[0]*4 for i in range(4)]
-    #for i in range(4):
-    #    nouvelle_mat.append([0] * 4)
-         
-    # avec toutes les cellules vides ici nous allons décaler les entrées
-    # de chaque cellule vers son extrémité gauche, rangée par rangée
-    
-    # Boucle pour parcourir les lignes
-    for i in range(4):
-        position = 0
- 
-        # boucle pour parcourir chaque colonne dans la ligne correspondante
-        for j in range(4):
-            if(mat[i][j] != 0):
-                 
-                # si la cellule n'est pas vide alors nous déplaçons son numéro vers
-                # la cellule vide précédente de cette ligne marqué par la variable position
-                nouvelle_mat[i][position] = mat[i][j]
-                 
-                if(j != position):
-                    change = True
-                position += 1
- 
-    # retour de la nouvelle matrice compressée et la variable de changement.
-    return nouvelle_mat, change
-#fonction pour fusionner les cellules dans la matrice après compression
+
 def fusion(mat):
      
     change = False
@@ -179,6 +145,7 @@ def transposition(mat):
 #def move_right
 
 #def reverese: fonction permettant d'inverser la matrice signifie inverser le contenu de chaque ligne 
+#def compress: fonction de compression de la grille après chaque étapes avant et après la fusion des cellules.
 
 #def score
 #def savegame
