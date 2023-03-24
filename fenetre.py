837c5dc89f99df9c55689ff88a7a0e6f1880588d
heyu12138 <120714180+heyu12138@users.noreply.github.com>
2023-03-24 10:00:41 +0100

    Add files via upload

--- fenetre.py
+++ fenetre.py
@@ -17,12 +17,9 @@ canvas.grid()
 
 for i in range(4):
     for j in range(4):
-       if (i+j) % 2 == 0:
-            color = "gray80"
-       else:
-            color = "gray80"
-            canvas.create_rectangle((i*largeur_case, j*hauteur_case),
-            ((i+1)*largeur_case, (j+1)*hauteur_case), fill=color)
+        color = "gray80"
+        canvas.create_rectangle((i*largeur_case, j*hauteur_case),
+        ((i+1)*largeur_case, (j+1)*hauteur_case), fill=color)
 
 bouton1 = tk.Button(fenetre, text="Nouveau Partie", font=("helvetica", "10"))
 bouton1.grid(column=1, row=0)
